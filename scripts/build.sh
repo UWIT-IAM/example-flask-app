51cf162e687a7a2f72f585ac5682e24c9fb123b3
Mike Richards <miker985@uw.edu>
2023-08-02 13:12:17 -0700

    Add trail of crumbs to easily remember what this command really does

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
+# `fingerprinter -o build-script` will always output the absolute path to this script
+# https://github.com/UWIT-IAM/fingerprinter/blob/main/fingerprinter/build.sh
 $(poetry run fingerprinter -o build-script) -p $@
