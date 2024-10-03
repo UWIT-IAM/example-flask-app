1cb61bfaa56939284a476368bd74bcf6c3515997
Kevin Counihan <counik@uw.edu>
2024-10-03 14:29:33 -0700

    cleanup globals a la netid_arrest

--- scripts/globals.template.sh
+++ scripts/globals.template.sh
@@ -4,12 +4,13 @@ export APP_NAME="${template:app_name}"
 
 function get_instance_status {
     local stage="$1"
-    local url="https://${APP_NAME}.iam${stage}.s.uw.edu/status"
-    curl -sk $url
+    # substitute '_' for '-' in APP_NAME
+    local url="https://${APP_NAME//_/-}.iam${stage}.s.uw.edu/status"
+    curl -sk "$url" || >&2 echo "Failed to check $url"
 }
 
 function get_poetry_version {
-  cat pyproject.toml | grep 'version =' | cut -f2 -d\" | head -n1
+  grep 'version =' pyproject.toml | cut -f2 -d\" | head -n1
 }
 
 
@@ -20,13 +21,13 @@ function get_promotion_version {
   local target=$1
   case $target in
     dev)
-      echo $(get_poetry_version)
+      get_poetry_version
       ;;
     eval)
-      echo $(get_instance_status dev | jq -r .version)
+      get_instance_status dev | jq -r .version
       ;;
     prod)
-      echo $(get_instance_status eval | jq -r .version)
+      get_instance_status eval | jq -r .version
       ;;
   esac
 }
